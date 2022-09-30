73cd28e2c297f1e9e50c18d17f87bb07d5a72f86
Jon Ander Oribe <jonan.oribe@gmail.com>
2022-09-30 15:09:54 +0200

    Customer view

--- crm/accounts/views.py
+++ crm/accounts/views.py
@@ -6,14 +6,31 @@ from .models import *
 def home(request):
     orders = Order.objects.all()
     customers = Customer.objects.all()
+    
+    total_customers = customers.count()
+    total_orders = orders.count()
+    delivered = orders.filter(status = 'Delivered').count()
+    pending = orders.filter(status = 'Pending').count()
+
     context = {
         'orders':orders,
-        'customers':customers
+        'customers':customers,
+        'total_customers':total_customers,
+        'total_orders':total_orders,
+        'delivered':delivered,
+        'pending':pending
     }
+
     return render(request,'accounts/dashboard.html',context)
 
-def customer(request):
-    return render(request,'accounts/customer.html')
+def customer(request,pk):
+    customer = Customer.objects.get(id=pk)
+    orders = customer.order_set.all()
+    orders_count = orders.count()
+    context = {'customer':customer,
+            'orders':orders,
+            'orders_count':orders_count}
+    return render(request,'accounts/customer.html',context)
 
 def products(request):
     products = Product.objects.all()
