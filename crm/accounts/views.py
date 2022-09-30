a45bb641f90bd009c651b778b1636fbe1dd27478
Jon Ander Oribe <jonan.oribe@gmail.com>
2022-09-30 18:50:51 +0200

    Order´s manager

--- crm/accounts/views.py
+++ crm/accounts/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
+
+from  .forms import OrderForm
 from .models import *
 # Create your views here.
 
@@ -35,3 +37,37 @@ def customer(request,pk):
 def products(request):
     products = Product.objects.all()
     return render(request,'accounts/products.html',{'products':products})
+
+def create_order(request):
+    form = OrderForm()
+
+    if request.method == 'POST':
+        form = OrderForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+
+    context={'form':form}
+    return render(request,'accounts/order_form.html',context)
+
+def update_order(request,pk):
+    order = Order.objects.get(id=pk)
+    form = OrderForm(instance=order)
+
+    if request.method == 'POST':
+        form = OrderForm(request.POST,instance=order)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+
+    context={'form':form}
+    return render(request,'accounts/order_form.html',context)
+
+def delete_order(request,pk):
+    order = Order.objects.get(id=pk)
+    if request.method == 'POST':
+        order.delete()
+        return redirect('/')
+        
+    context = {'item':order}
+    return render(request,'accounts/delete.html',context)
