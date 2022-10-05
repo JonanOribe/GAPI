01a9559b6f57f96ce9cc84ae64b55facc62cfa01
Jon Ander Oribe <jonan.oribe@gmail.com>
2022-10-05 12:42:08 +0200

    Login Logout

--- crm/accounts/views.py
+++ crm/accounts/views.py
@@ -6,6 +6,11 @@ from  .forms import OrderForm,CreateUserForm
 from .models import *
 from .filters import OrderFilter
 
+from django.contrib import messages
+from django.contrib.auth import authenticate,login,logout
+from django.contrib.auth.decorators import login_required
+
+@login_required(login_url='login')
 def home(request):
     orders = Order.objects.all()
     customers = Customer.objects.all()
@@ -26,6 +31,7 @@ def home(request):
 
     return render(request,'accounts/dashboard.html',context)
 
+@login_required(login_url='login')
 def customer(request,pk):
     customer = Customer.objects.get(id=pk)
     orders = customer.order_set.all()
@@ -40,10 +46,12 @@ def customer(request,pk):
             'myFilter': myFilter}
     return render(request,'accounts/customer.html',context)
 
+@login_required(login_url='login')
 def products(request):
     products = Product.objects.all()
     return render(request,'accounts/products.html',{'products':products})
 
+@login_required(login_url='login')
 def create_order(request,pk):
     OrderFormSet = inlineformset_factory(Customer,Order,fields=('product','status'),extra=4)
     customer = Customer.objects.get(id=pk)
@@ -57,6 +65,7 @@ def create_order(request,pk):
     context={'formset':formset}
     return render(request,'accounts/order_form.html',context)
 
+@login_required(login_url='login')
 def update_order(request,pk):
     order = Order.objects.get(id=pk)
     form = OrderForm(instance=order)
@@ -70,6 +79,7 @@ def update_order(request,pk):
     context={'form':form}
     return render(request,'accounts/order_form.html',context)
 
+@login_required(login_url='login')
 def delete_order(request,pk):
     order = Order.objects.get(id=pk)
     if request.method == 'POST':
@@ -80,17 +90,42 @@ def delete_order(request,pk):
     return render(request,'accounts/delete.html',context)
 
 def registerPage(request):
-    form = CreateUserForm()
+    if request.user.is_authenticated:
+        return redirect('home')
+    else:
+        form = CreateUserForm()
 
-    if request.method == 'POST':
-        form = CreateUserForm(request.POST)
-        if form.is_valid():
-            form.save()
+        if request.method == 'POST':
+            form = CreateUserForm(request.POST)
+            if form.is_valid():
+                form.save()
+                user = form.cleaned_data.get('username')
+                messages.success(request,'Account was created for ' + user)
+                return redirect('login')
 
-    context = {'form':form}
-    return render(request,'accounts/register.html',context)
-
+        context = {'form':form}
+        return render(request,'accounts/register.html',context)
 
 def loginPage(request):
-    context = {}
-    return render(request,'accounts/login.html',context)
+    if request.user.is_authenticated:
+        return redirect('home')
+    else:
+        context = {}
+        if request.method=='POST':
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+
+            user = authenticate(request,username=username,password=password)
+
+            if user is not None:
+                login(request,user)
+                return redirect('home')
+            else:
+                messages.info(request,'Username or Password is not correct!')
+                return render(request,'accounts/login.html',context)
+
+        return render(request,'accounts/login.html',context)
+
+def logoutUser(request):
+    logout(request)
+    return redirect('login')
